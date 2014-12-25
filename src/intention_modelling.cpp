d182f7d7de6c62e77dac8dbf8068822e61f3a967
akhil22 <akhil.nagariya@gmail.com>
2014-12-25 13:48:42 +0530

    intention_modelling.cpp subscibes to trajectory data

--- src/intention_modelling.cpp
+++ src/intention_modelling.cpp
@@ -2,11 +2,47 @@
 #include<stdio.h>
 #include<string>
 #include<geometry_msgs/Pose.h>
+#include<gaussian_process_catkin/covarianceFunctions.h>
 #include<visualization_msgs/MarkerArray.h>
 #include<visualization_msgs/Marker.h>
-//structure to conatin trajectory data
+
+//structure to contain trajectory data
 struct Trajectory{
 	int id;
 	int num_points;
+	TDoubleVector time;
+	TDoubleVector x;
+	TDoubleVector y;
+	TDoubleVector vx;
+	TDoubleVector vy;
+};
 
-int main(){}
+class IntentionModelling{
+	public:
+		IntentionModelling();
+	private:
+		// node handler
+		ros::NodeHandle nh_;
+		int sliding_window_size_;
+
+		//trajectory data subscriber
+		ros::Subscriber traj_sub_;
+
+		//trajectory subscriber call back function
+		void trajCB(const visualization_msgs::MarkerArray::ConstPtr& msg); 
+
+};
+
+IntentionModelling::IntentionModelling(){
+	ros::NodeHandle private_nh_("~");
+	private_nh_.param ("sliding_window_size", sliding_window_size_, int(4));
+	traj_sub_ = nh_.subscribe ("human_pose", 100, &IntentionModelling::trajCB, this);
+}
+
+void IntentionModelling::trajCB(const visualization_msgs::MarkerArray::ConstPtr& msg){
+	int num_markers = msg->markers.size();;
+}
+
+int main(){
+	return 0;
+}
