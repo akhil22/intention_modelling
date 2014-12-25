470eaae5724c7e5baa9c279f0a7291b975d100cb
akhil22 <akhil.nagariya@gmail.com>
2014-12-25 12:54:04 +0530

    add visualization_msgs as dependency

--- src/intention_modelling.cpp
+++ src/intention_modelling.cpp
@@ -2,5 +2,11 @@
 #include<stdio.h>
 #include<string>
 #include<geometry_msgs/Pose.h>
-using namespace ros;
+#include<visualization_msgs/MarkerArray.h>
+#include<visualization_msgs/Marker.h>
+//structure to conatin trajectory data
+struct Trajectory{
+	int id;
+	int num_points;
+
 int main(){}
